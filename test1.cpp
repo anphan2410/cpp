24b344a38e011dcaaafd6b0beeec45212288aeaf
anphan2410 <an.phan@ascenx.com>
2017-08-24 23:13:11 +0700

    First Commit

---
+++ test1.cpp
@@ -0,0 +1,12 @@
+#include "anlogger.h"
+using namespace std;
+int main()
+{
+	anTrk("This Is anTrk");
+	anAck("This Is aAck");
+	anInfo("Infomation Is Added");
+	anWarn("Warning Is Appeared !");
+	anError("Ohno Error Happens !");
+
+	return 0;
+}
